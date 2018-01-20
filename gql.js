329034a8c32364cf4b54baffead2be5c773b1835
Will Wright <wawwright@gmail.com>
2018-01-20 12:02:23 -0600

    added eslint and cleaned up resolvers

--- gql.js
+++ gql.js
@@ -13,60 +13,30 @@ const jq = require('node-jq');
 const typeDefs = /* GraphQL */`
     type Query {
       hello(name: String): String!
-      task(seed: Int!) : Task
-    }
-    type Mutation {
-      addTask(text: String!, parent: String): Task
     }
     # type User {
     #   id: ID
     #   name: String!
     #   work: [Work]
     # }
-    type Task {
-      id: ID
-      text: String!
-      isComplete: Boolean!
-      parent: Task
-      children: [Task]
-    }
   `;
 
 
 const id_ = () => faker.random.alphaNumeric(7);
 
-const getResolvers = (db) => {
+const getResolvers = () => {
 
   const res = {
     Query: {
       hello: (_, { name }) => `Hello ${name || 'World'}`,
-      task: (_, { id }) => { db.tasks.document(id); },
     },
-    Mutation: {
-      addTask: async (_, { text, parent = null }) => {
-        console.log('autenticating...');
-        console.log('saving...');
-        const task = await db.collection('tasks').save({ text });
-        console.log(task);
-        console.log('saved??');
-        if (!!parent && task) {
-          const child = task._id;
-          const parentId = `tasks/${parent}`;
-          console.log(`adding edge from ${parentId} to ${child}`);
-          const edge = await db.collection('taskEdges').save({ _from: parentId, _to: child });
-          console.log(edge);
-          console.log('sived edge?');
-        }
-        return task;
-      },
-    },
-    Task: {
-      id: id_,
-      text: () => faker.random.words(),
-      isComplete: () => faker.random.boolean(),
-      parent: () => (faker.random.arrayElement([null, {}])),
-      children: () => times(faker.random.arrayElement([0, 0, 1, 1, 1, 2, 2, 3]), () => ({})),
-    },
+    // Task: {
+    //   id: id_,
+    //   text: () => faker.random.words(),
+    //   isComplete: () => faker.random.boolean(),
+    //   parent: () => (faker.random.arrayElement([null, {}])),
+    //   children: () => times(faker.random.arrayElement([0, 0, 1, 1, 1, 2, 2, 3]), () => ({})),
+    // },
 
   };
 
@@ -76,11 +46,7 @@ const getResolvers = (db) => {
 
 async function run() {
 
-  const db = new Database();
-  db.useDatabase('todos');
-  db.useBasicAuth('root', '');
-
-  const resolvers = getResolvers(db);
+  const resolvers = getResolvers();
   const server = new GraphQLServer({ typeDefs, resolvers });
   server.start(() => console.log('Server is running on localhost:4000'));
 }
